f765464a075de7ceb56209e872f0af40ffea58c0
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2025-09-28 14:32:13 +0200

    dodany szablon blade do widoków

---
+++ resources/views/kontakt.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.layout')
+@section('tytul','Kontakt')
+@section('podtytul', 'Strona kontaktowa')
+@section('tresc')
+    <div>
+        Treść strony kontaktowej <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perferendis sunt sed nulla, officia neque autem tempore cum nihil ipsum quasi maiores dignissimos quo cumque deleniti deserunt ipsam praesentium molestias?
+    </div>
+@endsection
---
+++ resources/views/onas.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.layout')
+@section('tytul','O nas')
+@section('podtytul', 'Strona o nas')
+@section('tresc')
+    <div>
+        Treść dla strony o nas <br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repellat iste placeat, deserunt itaque esse eveniet dicta vitae quidem aliquid in quam maxime. Nesciunt alias consequuntur numquam, blanditiis esse voluptate.
+    </div>
+@endsection
+
